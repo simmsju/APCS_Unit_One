ae5bc72d2d4872226ff4eeb7eb58fe9f1e172bc4
simmsju <simmsju@ssfs.org>
2021-09-14 15:14:46 -0400

    Continued User Input

--- src/MetaJava.java
+++ src/MetaJava.java
@@ -1,9 +1,9 @@
 public class MetaJava {
     public static void main(String[] args) {
         System.out.println("public class Hello {");
-        System.out.println("\tpublic static void main(String[] args) {");
-        System.out.println("\t\tSystem.out.println(\"Hello, world!\");");
-        System.out.println("\t}");
+        System.out.println("    public static void main(String[] args) {");
+        System.out.println("        System.out.println(\"Hello, world!\");");
+        System.out.println("    }");
         System.out.println("}");
     }
 }
--- src/SeparatingDigits.java
+++ src/SeparatingDigits.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class SeparatingDigits {
-
     public static void main(String[] args) {
-
+        int n1, n2, n3, n4, n5;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a five digit whole number:");
+        int number = scan.nextInt();
+        n5 = number % 10;
+        n4 = number % 100 / 10;
+        n3 = number % 1000 / 100;
+        n2 = number % 10000 / 1000;
+        n1 = number % 100000 / 10000;
+        System.out.println(n1 + "   " + n2 + "   " + n3 + "   " + n4 + "   " + n5);
     }
 }
